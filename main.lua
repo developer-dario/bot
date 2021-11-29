f11a0213f1af1b4418f494236f8a1f8197d2cffa
ingressboy <81222444+ingressboy@users.noreply.github.com>
2021-11-29 12:18:06 +0100

    Grund + moin befehl

--- main.lua
+++ main.lua
@@ -1,1 +1,16 @@
 ---Ein kleiner Bot ---
+
+local discordia = require('discordia')
+local client = discordia.Client()
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.content == 'moin' then
+		message.channel:send('Moin!')
+	end
+end)
+
+client:run('Bot INSERT_TOKEN_HERE')
